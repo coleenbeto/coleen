5ad6689d9a11e7cbb8257df588131c5af148d30d
coleenbeto <beatrixbeto@sec.edu.ph>
2024-05-30 14:40:22 +0800

    Add files via upload

---
+++ assets/demo/chart-pie-demo.js
@@ -0,0 +1,28 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: ["JAN", "FEB", "MAR", "APR"],
+    datasets: [{
+      data: [39, 76, 46, 66],
+     backgroundColor: ['rgba(9, 171, 11, 0.1)',// Green
+	'rgba(58, 9, 171, 0.1)',// Violet
+	'rgba(210, 226, 14, 0.1)', // Yellow
+	'rgba(255, 0, 0, 0.1)', // Red
+	'rgba(255, 0, 247, 0.1)', // Pink
+	],
+    borderColor: ['rgba(9, 171, 11, 0.5)',// Green
+	'rgba(58, 9, 171, 0.5)',// Violet
+	'rgba(210, 226, 14, 0.5)', // Yellow
+	'rgba(255, 0, 0, 0.5)', // Red
+	'rgba(255, 0, 247,0.5)', // Pink
+	],
+    borderWidth: 1
+  }]
+  },
+});
---
+++ lab5/Chart.js
@@ -0,0 +1,48 @@
+const body = document.body;
+body.style.backgroundColor = 'rgba(26, 117, 255, 0.15)'; 
+body.style.backgroundImage = 'https://img.freepik.com/free-photo/blue-smoke-background-textured-wallpaper-high-resolution_53876-128239.jpg?size=626&ext=jpg&ga=GA1.1.553209589.1713916800&semt=ais';
+const sampleData = {
+  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+  datasets: [{
+    label: 'Sample Data',
+    data: [30, 89, 100, 56, 26, 58, 21, 67, 12, 43, 76, 54],
+    backgroundColor: ['rgba(9, 171, 11, 0.1)',// Green
+	'rgba(58, 9, 171, 0.1)',// Violet
+	'rgba(210, 226, 14, 0.1)', // Yellow
+	'rgba(255, 0, 0, 0.1)', // Red
+	'rgba(255, 0, 247, 0.1)', // Pink
+	],
+    borderColor: ['rgba(9, 171, 11, 0.5)',// Green
+	'rgba(58, 9, 171, 0.5)',// Violet
+	'rgba(210, 226, 14, 0.5)', // Yellow
+	'rgba(255, 0, 0, 0.5)', // Red
+	'rgba(255, 0, 247,0.5)', // Pink
+	],
+    borderWidth: 1
+  }]
+};
+
+const ctx = document.getElementById('myChart').getContext('2d');
+let currentChart;
+
+function renderChart(type) {
+  if (currentChart) {
+    currentChart.destroy();
+  }
+  
+  currentChart = new Chart(ctx, {
+    type: type,
+    data: sampleData,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
+function changeChartType(type) {
+  renderChart(type);
+}
+renderChart('bar')
